f266fcd921d218350354dc3d6346717302e513e2
deepak-kumar65 <deepakk@leewayhertz.com>
2022-07-29 19:01:36 +0530

    updated build

---
+++ build/precache-manifest.e307b6881ae4936babd5bba1b5c217f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f5be82c0df734ff01b908b24e29054",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec948a26d36215d146c3",
+    "url": "/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "9afa7625287ee586b282",
+    "url": "/static/css/main.33725f5b.chunk.css"
+  },
+  {
+    "revision": "ec948a26d36215d146c3",
+    "url": "/static/js/2.abbaf51c.chunk.js"
+  },
+  {
+    "revision": "9afa7625287ee586b282",
+    "url": "/static/js/main.f5e50361.chunk.js"
+  },
+  {
+    "revision": "38b5775939135e670be6",
+    "url": "/static/js/runtime~main.d3edf892.js"
+  }
+]);
